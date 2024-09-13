b09a6057e3b3c4fe34860802e346eaf014b4d9eb
omid <imguromid@gmail.com>
2024-09-13 23:22:03 +0430

    feature: adding interface map to the app

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,22 +1,96 @@
+import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from 'react-leaflet';
+import { LatLngExpression, LatLngTuple } from 'leaflet';
+
+import { useGeolocation } from './../hooks/useGeolocation';
+import { useCities } from '../contexts/CitiesContext';
 import styles from './Map.module.css';
+import Button from './Button';
 
 function Map() {
-  const navigate = useNavigate()
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  const [position, setPosition] = useState([40, 0] as LatLngTuple);
+  const { cities } = useCities();
+  const {
+    isLoading: isLoadingPosition,
+    position: userPosition,
+    getPosition,
+  } = useGeolocation();
+
+  console.log(userPosition);
+  const lat = Number(searchParams.get('lat'));
+  const lng = Number(searchParams.get('lng'));
+
+  useEffect(
+    function () {
+      if (lat !== 0 && lng !== 0) setPosition([lat, lng]);
+    },
+    [lat, lng]
+  );
+
+  useEffect(
+    function () {
+      if (userPosition) setPosition([userPosition.lat, userPosition.lng]);
+    },
+    [userPosition]
+  );
 
   return (
-    <div className={styles.mapContainer} onClick={()=>{
-      navigate('form')
-    }}>
-      <h1>map</h1>
-      <h1>
-        position: {lat}, {lng}
-      </h1>
+    <div className={styles.mapContainer}>
+      {!userPosition && (
+        <Button type='position' onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use yor position'}
+        </Button>
+      )}
+      <MapContainer center={position} zoom={6} className={styles.map}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        {cities.map((city) => (
+          <Marker
+            position={
+              [city.position.lat, city.position.lng] as LatLngExpression
+            }
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName} </span>
+            </Popup>
+          </Marker>
+        ))}
+
+        <ChangeCenter position={position} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }: { position: LatLngTuple }) {
+  const map = useMap();
+  const [lat, lng] = position;
+  map.setView([lat || 0, lng || 0]);
+
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => navigate(`form/?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+
+  return null;
 }
 
 export default Map;
